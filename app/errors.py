eb8c49111daa8bf85eb83521307e62591a5baddd
dmytro0908 <gavrildima42@gmail.com>
2025-03-14 15:50:56 +0200

    Solution

---
+++ app/errors.py
@@ -0,0 +1,15 @@
+# app/errors.py
+class VaccineError(Exception):
+    pass
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    pass
+
+
+class NotWearingMaskError(Exception):
+    pass
